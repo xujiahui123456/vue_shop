dbe7037ed7458345723ae9fcae03916ba0365937
徐家汇 <1504881098@qq.com>
2020-05-12 23:04:25 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      // 编程式导航
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区域 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt />
+      </div>
+      <!-- 登录表单区域 -->
+      <el-form ref="loginFormRef" :rules="loginFormRules" :model="loginForm" label-width="0px" class="login_form">
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input
+            v-model="loginForm.password"
+            prefix-icon="iconfont icon-3702mima"
+            type="password"
+          ></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="warning" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // 这是登录表单的数据绑定对象
+      loginForm: {
+        username: 'zs',
+        password: '123'
+      },
+      // 这是表单的验证规则对象
+      loginFormRules: {
+        username: [
+          // 验证用户名是否合法
+          { required: true, message: '请输入登录名称', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入登录密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    // 点击重置按钮，重置登录表单
+    resetLoginForm () {
+      this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      this.$refs.loginFormRef.validate(async (valid) => {
+        if (valid) {
+          // {data:res}解构赋值
+          const { data: res } = await this.$http.post('login', this.loginForm)
+          if (res.meta.status !== 200) {
+            return this.$message.error('登陆失败')
+          } else {
+            this.$message.success('登录成功')
+            // 1将登陆成功之后的 token,保存到客户端的 sessionStorage中
+            // 1.1项目中处理登录之外的其他API接口，必须在登陆之后才能访问
+            // 1.2token只应在当前网站打开期间生效，所以将token保存在sessionStorage中
+            console.log(res)
+            window.sessionStorage.setItem('token', res.data.token)
+            // 2.通过编程式导航跳转到后台主页，路由地址是/home
+            this.$router.push('/home')
+          }
+        } else {
+          console.log('验证不通过')
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #ffd8a6;
+  border-radius: 5px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #ffd8a6;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>

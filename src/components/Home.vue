46594681d1365d2ea5122c50027efbe3d70ef9c1
徐家汇 <1504881098@qq.com>
2020-05-13 16:31:59 +0800

    侧边栏功能完成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,7 @@
     <!-- 头部区域 -->
     <el-header>
       <div>
-        <img src="../assets/heima.png" alt="">
+        <img src="../assets/heima.png" alt />
         <span>电商后台管理系统</span>
       </div>
       <el-button type="info" @click="logout">退出</el-button>
@@ -11,20 +11,79 @@
     <!-- 页面主体区域 -->
     <el-container>
       <!-- 侧边栏 -->
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside :width="isCollapse?'64px' :'200px' ">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <!-- 侧边栏菜单区 -->
+        <el-menu background-color="#333744" text-color="#fff" active-text-color="#409eff" unique-opened
+         :collapse="isCollapse" :collapse-transition='false' :router="true">
+          <!-- 一级菜单 -->
+          <el-submenu :index="item.id+''" v-for="item in menulist" :key="item.id">
+            <!-- 一级菜单的模板区 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i :class="iconsObj[item.id]" class="iconMain"></i>
+              <!-- 文本 -->
+              <span>{{item.authName}}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item :index="'/'+subItem.path+''" v-for="subItem in item.children" :key="subItem.id">
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-menu"></i>
+                <!-- 文本 -->
+                <span>{{subItem.authName}}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <!-- 右侧内容主体 -->
-      <el-main>Main</el-main>
+      <el-main>
+        <!-- 路由占位符 -->
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      // 左侧菜单数据
+      menulist: [],
+      iconsObj: {
+        125: 'iconfont icon-user',
+        103: 'iconfont icon-tijikongjian',
+        101: 'iconfont icon-shangpin',
+        102: 'iconfont icon-danju',
+        145: 'iconfont icon-baobiao'
+      },
+      // 是否折叠
+      isCollapse: false
+    }
+  },
+  created() {
+    this.getMenulist()
+  },
   methods: {
     logout() {
       window.sessionStorage.clear()
       // 编程式导航
       this.$router.push('/login')
+    },
+    // 获取所有的菜单
+    async getMenulist() {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) {
+        return this.$message.error(res.meta.msg)
+      }
+      this.menulist = res.data
+      console.log(res)
+    },
+    // 点击按钮，切换菜单的折叠与展开
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse
     }
   }
 }
@@ -40,9 +99,9 @@ export default {
   justify-content: space-between;
   padding-left: 0;
   align-items: center;
-  color: rgba(80,175,239,1);
+  color: rgba(80, 175, 239, 1);
   font-size: 20px;
-  >div {
+  > div {
     display: flex;
     align-items: center;
     span {
@@ -51,12 +110,30 @@ export default {
   }
 }
 .el-button {
-  color: rgba(80,175,239,1);
+  color: rgba(80, 175, 239, 1);
 }
 .el-aside {
   background-color: #333744;
+  .el-menu {
+    border-right: none;
+  }
 }
+
+.iconMain {
+  margin-right: 5px;
+}
+
 .el-main {
   background-color: #eaedf1;
+}
+
+.toggle-button {
+  background-color: #4a5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -9,6 +9,12 @@ import './assets/css/global.css'
 // 导入axios
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 请求拦截器
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须return config
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false

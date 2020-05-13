8b4a70de9bfd3bea83945f86148a314ac9ca67c8
徐家汇 <1504881098@qq.com>
2020-05-13 11:24:59 +0800

    项目头部完成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,27 @@
 <template>
-    <div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" alt="">
+        <span>电商后台管理系统</span>
+      </div>
       <el-button type="info" @click="logout">退出</el-button>
-    </div>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">Aside</el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
   methods: {
-    logout () {
+    logout() {
       window.sessionStorage.clear()
       // 编程式导航
       this.$router.push('/login')
@@ -17,5 +31,32 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: rgba(80,175,239,1);
+  font-size: 20px;
+  >div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+.el-button {
+  color: rgba(80,175,239,1);
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
 </style>
